0a4dddfe4f4f872e5a4d1b54e25e3bf5bf7fbeb1
wei.mo@fangbaba.com <wei.mo@fangbaba.com>
2017-12-13 14:03:18 +0800

    优化

--- service-ribbon/src/main/java/com/murphy/springboot/service/DemoService.java
+++ service-ribbon/src/main/java/com/murphy/springboot/service/DemoService.java
@@ -2,6 +2,7 @@ package com.murphy.springboot.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +18,9 @@ public class DemoService {
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${service-one.domain}")
+    String serviceOneDomain;
+
     /**
      * 当service-one服务不可用或者线程堵塞时，SpringCloud使用Hystrix组件实现断路器模式，保证调用者不会因此而瘫痪。
      * 如果不采用断路器，由于服务与服务之间的依赖性，故障会传播，会对整个微服务系统造成灾难性的严重后果，这就是服务故障的“雪崩”效应。
@@ -27,11 +31,11 @@ public class DemoService {
      */
     @HystrixCommand(fallbackMethod = "callServiceError")
     public String callServiceOne(String name) {
-        return "to call service one, return: " + restTemplate.getForObject("http://service-one/?name=" + name, String.class);
+        return "to call service one, return: " + restTemplate.getForObject(serviceOneDomain + "?name=" + name, String.class);
     }
 
     public String callServiceError(String name) {
-        return "oh restTemplate+ribbon call " + name + " error!";
+        return "oh restTemplate call " + name + " error!";
     }
 
 }
